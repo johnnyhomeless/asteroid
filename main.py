2b84f13b00f0014dcbeee17e9e4e9fdec45cd9b9
johnnynocash <giovolo@gmail.com>
2024-11-06 10:40:27 +0100

    lessgoooo

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from circleshape import CircleShape
+import player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_instance = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000
+        for x in updatable:
+            x.update(dt)
+        for x in drawable:
+            x.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-            return
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
