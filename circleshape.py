86e0a4784ec9a74bae70c52af9e098cf486734f1
johnnynocash <giovolo@gmail.com>
2024-11-08 08:37:21 +0100

    Draw Player

--- circleshape.py
+++ circleshape.py
@@ -1,9 +1,7 @@
 import pygame
-from constants import *
 
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -14,14 +12,7 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
         pass
 
     def update(self, dt):
-        # sub-classes must override
         pass
-
-    def check_collision(self, other_shape):
-        distance = self.position.distance_to(other_shape.position)
-        return distance <= self.radius + other_shape.radius
-        
