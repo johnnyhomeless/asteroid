1b15d99f6a0c9ef04b60510c1cc66675f1c87566
johnnynocash <giovolo@gmail.com>
2024-11-10 14:45:02 +0100

    DRAW PLAYER

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from  player import Player
 
 def main():
     print("Starting asteroids!")
@@ -12,9 +13,12 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         new_dt = clock.tick(60)
