e3a02ec4fa2cb82543289540d1617fcf1724e51d
johnnynocash <giovolo@gmail.com>
2024-11-13 07:07:15 +0100

    Created text_manager.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# main.py
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from text_manager import TextManager
 import sys
 
 def main():
@@ -16,17 +18,8 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # Load the logo font
-    logo_font = pygame.font.Font("fonts/INVASION2000.TTF", 96)
-    logo_text = logo_font.render("AstroCazzo", True, (255, 255, 255))
-    logo_rect = logo_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/4))
-
-    game_over_font = pygame.font.Font("fonts/ARCADECLASSIC.TTF", 74)
-    game_over_text = game_over_font.render('Game Over!', True, (255, 255, 255))
-    game_over_rect = game_over_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-
+    text_manager = TextManager()
     sprites = pygame.sprite.Group()
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
    
@@ -36,8 +29,6 @@ def main():
     game_over = False
     show_start_message = True
     show_logo = True
-    start_message_visible = True
-    start_message_timer = 0.5  # Time (in seconds) to show/hide the start message
 
     while running:
         for event in pygame.event.get():
@@ -53,39 +44,27 @@ def main():
             asteroidfield.update(dt)
     
         for asteroid in asteroidfield.asteroids:
-             if player.collision(asteroid):
-                 print("Game over!")
-                 game_over = True
+            if player.collision(asteroid):
+                print("Game over!")
+                game_over = True
 
         screen.fill((0, 0, 0))
-        if show_logo:
-            screen.blit(logo_text, logo_rect)
 
         if show_start_message:
-            start_font = pygame.font.Font("fonts/ARCADECLASSIC.TTF", 36)
-            if start_message_visible:
-                start_text = start_font.render("Press SPACE to start", True, (255, 255, 255))
-            else:
-                start_text = start_font.render("Press SPACE to start", True, (0, 0, 0))  # Make the text invisible
-            start_rect = start_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT - 100))
-            screen.blit(start_text, start_rect)
-
-            start_message_timer -= dt
-            if start_message_timer <= 0:
-                start_message_visible = not start_message_visible
-                start_message_timer = 0.5
+            text_manager.update_start_message(dt)
 
         if not game_over and not show_start_message:
             sprites.draw(screen)
             asteroidfield.asteroids.draw(screen)
-        elif game_over:
-            screen.blit(game_over_text, game_over_rect)
+        
+        text_manager.draw(screen, show_logo, show_start_message, game_over)
+
+        if game_over:
             pygame.display.flip()
             pygame.time.wait(2000)
             running = False
 
         pygame.display.flip()     
-
         dt = clock.tick(60) / 1000.0
 
     pygame.quit()
---
+++ text_manager.py
@@ -0,0 +1,41 @@
+# text_manager.py
+import pygame
+from constants import *
+
+class TextManager:
+    def __init__(self):
+        # Load fonts
+        self.logo_font = pygame.font.Font("fonts/INVASION2000.TTF", 96)
+        self.game_font = pygame.font.Font("fonts/ARCADECLASSIC.TTF", 74)
+        self.start_font = pygame.font.Font("fonts/ARCADECLASSIC.TTF", 36)
+
+        # Create text surfaces
+        self.logo_text = self.logo_font.render("AstroCazzo", True, (255, 255, 255))
+        self.logo_rect = self.logo_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/4))
+
+        self.game_over_text = self.game_font.render('Game Over!', True, (255, 255, 255))
+        self.game_over_rect = self.game_over_text.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+        # Start message state
+        self.start_message_visible = True
+        self.start_message_timer = 0.5
+        self.start_text_visible = self.start_font.render("Press SPACE to start", True, (255, 255, 255))
+        self.start_text_invisible = self.start_font.render("Press SPACE to start", True, (0, 0, 0))
+        self.start_rect = self.start_text_visible.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT - 100))
+
+    def update_start_message(self, dt):
+        self.start_message_timer -= dt
+        if self.start_message_timer <= 0:
+            self.start_message_visible = not self.start_message_visible
+            self.start_message_timer = 0.5
+
+    def draw(self, screen, show_logo=False, show_start_message=False, game_over=False):
+        if show_logo:
+            screen.blit(self.logo_text, self.logo_rect)
+
+        if show_start_message:
+            start_text = self.start_text_visible if self.start_message_visible else self.start_text_invisible
+            screen.blit(start_text, self.start_rect)
+
+        if game_over:
+            screen.blit(self.game_over_text, self.game_over_rect)
